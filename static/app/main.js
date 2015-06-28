1f13a84e4c9c75ea353209100261e944a7cd8030
chanaka_de_silva <chanaka@srilanka>
2015-06-28 19:19:12 +0530

    Add Session Feature Added to the System

--- static/app/main.js
+++ static/app/main.js
@@ -1,4 +1,4 @@
-var module = angular.module('cloud_accounting', ['ngRoute', 'chart.js']);
+var module = angular.module('eventManager', ['ngRoute', 'chart.js']);
 
 
 module.config(function ($routeProvider) {
@@ -9,33 +9,6 @@ module.config(function ($routeProvider) {
             controller: ''
         }).when('/income', {
             templateUrl: 'static/app/template/income/Income.html',
-            controller: ''
-        }).when('/spending', {
-            templateUrl: 'static/app/template/spendings/Spending.html',
-            controller: ''
-        }).when('/customer', {
-            templateUrl: 'static/app/template/customer/Customer.html',
-            controller: ''
-        }).when('/invoice/:customer_id', {
-            templateUrl: 'static/app/template/invoice/Invoice.html',
-            controller: ''
-        }).when('/report', {
-            templateUrl: 'static/app/template/reports/Report.html',
-            controller: ''
-        }).when('/payment', {
-            templateUrl: 'static/app/template/payments/Payment.html',
-            controller: ''
-        }).when('/inventory', {
-            templateUrl: 'static/app/template/inventry/Inventry.html',
-            controller: ''
-        }).when('/purchase', {
-            templateUrl: 'static/app/template/Purchase/Purchase.html',
-            controller: ''
-        }).when('/purchase_view', {
-            templateUrl: 'static/app/template/Purchase/PurchaseView.html',
-            controller: ''
-        }).when('/grn', {
-            templateUrl: 'static/app/template/Grn/GRN.html',
             controller: ''
         }).otherwise({
             redirectTo: '/'
