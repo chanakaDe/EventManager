1951a77a25cc28053bfb17fcfaadda2a825daebb
chanaka_de_silva <chanaka@srilanka>
2015-06-29 19:36:02 +0530

    Added Speaker Management Features

---
+++ static/app/controller/SpeakerController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by chanaka on 6/29/15.
+ */
+function SpeakerController($scope, speakerService) {
+
+}
--- static/app/url.js
+++ static/app/url.js
@@ -6,5 +6,6 @@ var mainHost = "http://192.168.1.3:8080/web/mintbooks/v1";
 
 var host = {
     user: mainHost + '/user',
-    session : mainHost + '/session'
+    session : mainHost + '/session',
+    speaker : mainHost + '/speaker'
 };
